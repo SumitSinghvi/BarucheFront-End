886da130d200fb553fcd48b9b4c09001685a157a
Sumit Singhvi <seemadavi78@example.com>
2024-02-01 14:23:33 +0530

    v1

--- src/pages/product/ProductLayout.jsx
+++ src/pages/product/ProductLayout.jsx
@@ -24,11 +24,12 @@ const ProductLayout = () => {
 
     };
 
-    const handleAddItem = async (id, quantity) => {
+    const handleAddItem = async (productID, quantity) => {
         try {
-            const cart = await addToCart({ variantId: id, quantity: quantity });
+            const cart = await addToCart({ variantId: productID, quantity: quantity });
             console.log(cart);
             console.log("Added to cart");
+            location.reload();
         } catch (error) {
             console.error("Error adding to cart:", error);
         }
